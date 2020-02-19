a3d058403b5406d023125eb33373054748041063
harika <harikapuppala19@gmail.com>
2020-02-19 10:49:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express")
+var app = express()
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+   res.render("home");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/booksearch", (req, res) => {
+    res.render("booksearch");
+});
+
+app.get("/bookissue", (req, res) => {
+    res.render("bookissue");
+});
+
+app.get("/login", )
+
+app.post("/register", function(req, res){
+     console.log(req.body);
+});
+
+app.listen(3000, function(req, res){
+    console.log("started");
+});
